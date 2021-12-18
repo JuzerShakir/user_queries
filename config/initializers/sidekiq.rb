226145b2bc56b40b8cb0674af62c10035b565c40
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-12-18 23:07:47 +0530

    typo fix

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,6 +1,6 @@
 Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://127.0.0.1:6379' }
-  if Rails.environment.production?
+  if Rails.env.production?
     uri = URI.parse(ENV['REDIS_URL'])
     REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
     config.redis = REDIS
@@ -10,7 +10,7 @@ end
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis://127.0.0.1:6379' }
 
-  if Rails.environment.production?
+  if Rails.env.production?
     uri = URI.parse(ENV['REDIS_URL'])
     REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
     config.redis = REDIS
