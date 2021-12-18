4fa1f500abb92378a3bd402207416352a6090198
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-12-18 22:33:55 +0530

    bug fix for logged out user

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @search_result = SearchResult.new
-    @searches = current_user.search_results
+    @searches = current_user.search_results if current_user
   end
 end
