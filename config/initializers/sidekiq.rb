6783c2b3dbae5c72c0f32055cdc9f41bbe035864
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-12-18 23:49:39 +0530

    Remove constant

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -2,8 +2,7 @@ Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://127.0.0.1:6379' }
   if Rails.env.production?
     uri = URI.parse(ENV['REDIS_URL'])
-    REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
-    config.redis = REDIS
+    config.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
   end
 end
 
@@ -12,7 +11,6 @@ Sidekiq.configure_server do |config|
 
   if Rails.env.production?
     uri = URI.parse(ENV['REDIS_URL'])
-    REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
-    config.redis = REDIS
+    config.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
   end
 end
