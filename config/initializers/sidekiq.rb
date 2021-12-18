5d047afc6cc834eb7059a9055b3e1e371eb4ac80
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-12-18 23:05:25 +0530

    Setup Redis connection

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,18 @@
 Sidekiq.configure_client do |config|
   config.redis = { url: 'redis://127.0.0.1:6379' }
+  if Rails.environment.production?
+    uri = URI.parse(ENV['REDIS_URL'])
+    REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+    config.redis = REDIS
+  end
 end
 
 Sidekiq.configure_server do |config|
   config.redis = { url: 'redis://127.0.0.1:6379' }
+
+  if Rails.environment.production?
+    uri = URI.parse(ENV['REDIS_URL'])
+    REDIS = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+    config.redis = REDIS
+  end
 end
