52bec6f6fcc89c2694aae3e575ad345c6f75ad7b
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-12-18 23:51:52 +0530

    Fixed logic

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,16 +1,17 @@
 Sidekiq.configure_client do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379' }
   if Rails.env.production?
     uri = URI.parse(ENV['REDIS_URL'])
     config.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+  else
+    config.redis = { url: 'redis://127.0.0.1:6379' }
   end
 end
 
 Sidekiq.configure_server do |config|
-  config.redis = { url: 'redis://127.0.0.1:6379' }
-
   if Rails.env.production?
     uri = URI.parse(ENV['REDIS_URL'])
     config.redis = Redis.new(host: uri.host, port: uri.port, password: uri.password)
+  else
+    config.redis = { url: 'redis://127.0.0.1:6379' }
   end
 end
