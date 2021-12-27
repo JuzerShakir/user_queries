56f995a6e10d3bcbc2aba61f429e61616e3bf9db
Juzer Shakir <juzershakir.webdev@gmail.com>
2021-12-27 14:04:50 +0530

    Show keywords in descending order at the time they were created

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,6 +1,6 @@
 class HomeController < ApplicationController
   def index
     @search_result = SearchResult.new
-    @searches = current_user.search_results if current_user
+    @searches = current_user.search_results.order(created_at: :desc) if current_user
   end
 end
